e4d85301728afedf84c38cf7d20c53372cd87cdb
asus <abir.chebbi@supcom.tn>
2021-04-05 16:01:07 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React from "react";
+import Joueurs from "./Pages/joueurs/Joueurs";
+import Home from "./Pages/home/Home";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      {/*<Joueurs/>*/}
+     <Home/>
     </div>
   );
 }
